48a8a2c6560eb06dd5a7acffe5ca42fcfa5bc2a5
Oliver Harrison <olivermharrison@gmail.com>
2018-08-15 22:02:31 +0100

    [+] Sidenav link for the pickers page

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -11,6 +11,7 @@ module.exports = {
             ['/scheduling', 'Scheduling'],
             ['/tabs', 'Tabbed Navigation'],
             ['/authentication', 'Authentication'],
+            ['/pickers', 'Pickers'],
         ]
     }
 }
