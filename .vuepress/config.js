934e1836f09543f181d7f50ab68cea08f3138995
Oliver Harrison <olivermharrison@gmail.com>
2019-10-16 21:34:04 +0100

    [~] vuepress config

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,15 +1,17 @@
 module.exports = {
     title: 'Android/Kotlin Cheat Sheet',
-    description: 'A collection of code snippets for common features in my Android apps',
+    description: 'A collection of code snippets for common features in Android apps',
     themeConfig: {
+        sidebarDepth: 3,
+        displayAllHeaders: true,
         sidebar: [
             ['/', 'Home'],
-            ['/activities', 'Activities'],
             ['/appbar', 'The App Bar'],
+            ['/tabs', 'Tabbed Navigation'],
             ['/lists', 'Lists'],
+            ['/activities', 'Activities'],
             ['/notifications', 'Notifications'],
             ['/scheduling', 'Scheduling'],
-            ['/tabs', 'Tabbed Navigation'],
             ['/authentication', 'Authentication'],
             ['/pickers', 'Pickers'],
             ['/room', 'Room Database'],
